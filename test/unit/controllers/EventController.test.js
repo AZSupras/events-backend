175343cdc8f12ca50cfbc48a6906c218b8c189b2
Mike DeVita <mike@relative.media>
2015-11-24 14:46:44 -0700

    add in test stubs for admin related functions
    Signed-off-by: Mike DeVita <mike@relative.media>

--- test/unit/controllers/EventController.test.js
+++ test/unit/controllers/EventController.test.js
@@ -2,7 +2,23 @@
 var faker = require('faker');
 var request = require('supertest');
 
-describe.only('EventController', function (){
+describe('EventController', function (){
+
+
+  describe('create', function () {
+    it('given a valid token, it should create an event');
+    it('given a valid token and invalid data, it should not create an event');
+  });
+
+  describe('edit', function () {
+    it('given a valid token, it should edit an event');
+    it('given a valid token and invalid data, it should not edit an event');
+  });
+
+  describe('delete', function (){
+    it('given a valid token, it should delete an event');
+    it('given a valid token and invalid data, it should not delete an event');
+  });
 
   describe('find', function (){
     it('should return an array of events', function (done){
@@ -12,7 +28,9 @@ describe.only('EventController', function (){
       .set('Accept', 'application/json')
       .expect(200)
       .expect(function (res){
-        res.body.should.be.an.Array();
+        res.body.should.be.an.Object();
+        res.body.should.have.properties('results', 'meta');
+        res.body.results.should.be.an.Array();
       })
       .end(function (err){
         if (err){
