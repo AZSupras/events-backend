796eff62c4fb104faf387025bac3f6a61cc38520
Mike DeVita <mike@relative.media>
2016-01-21 09:21:32 -0700

    remove the sensitive data from config for email
    Signed-off-by: Mike DeVita <mike@relative.media>

--- config/email.js
+++ config/email.js
@@ -1,11 +1,6 @@
 module.exports.email = {
   service: 'Mandrill',
-  auth: {
-    user: 'mike@gorelative.com',
-    pass: 'l7SRHmdxxFuIxL_jQ8tUeg'
-  },
   templateDir: 'views/email',
   from: 'noreply@azsupras.com',
-  to: 'mike@relative.media',
   testMode: false
 };
